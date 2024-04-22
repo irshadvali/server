355b1de3b8bfa3987e48056103878e6997612e2a
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2024-04-22 15:06:01 +0530

    server

--- server.js
+++ server.js
@@ -22,29 +22,29 @@ const jsonData = {
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-    // Set response headers
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    });
-
     // Parse the request URL
     const urlParts = req.url.split('/');
     const id = urlParts[urlParts.length - 1];
 
     // Check if id exists in jsonData
     if (jsonData[id]) {
+        // Set response headers
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        });
         // Send JSON data for the id
         res.end(JSON.stringify(jsonData[id]));
     } else {
-        // Send error if id is not found
+        // Set response headers for 404
         res.writeHead(404, { 'Content-Type': 'text/plain' });
+        // Send error if id is not found
         res.end('Data not found');
     }
 });
 
 // Start the server
-const port = 3000;
+const port = 3501;
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
